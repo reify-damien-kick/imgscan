44c64473df5d7805e84f8fe9515e90b2d50764c3
Damien Kick <dkick1@mac.com>
2022-02-02 09:17:38 -0600

    django-admin startproject

---
+++ py/django/imgscan/imgscan/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for imgscan project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imgscan.settings')
+
+application = get_wsgi_application()
