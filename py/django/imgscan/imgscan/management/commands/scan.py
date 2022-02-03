a8921c3d5692de33405db2c3b050ff6d29d7197f
Damien Kick <dkick1@mac.com>
2022-02-03 13:43:43 -0600

    Add scan command

--- py/django/imgscan/imgscan/management/commands/scan.py
+++ py/django/imgscan/imgscan/management/commands/scan.py
@@ -1,9 +1,11 @@
 from django.core.management.base import BaseCommand
 
+from imgscan.core import safe_update_images
+
 
 class Command(BaseCommand):
     help = 'Uses google-cloud-vision to scan for objects in images'
 
     def handle(self, *args, **kwargs):
-        pass
+        safe_update_images()
   
