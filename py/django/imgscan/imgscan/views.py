68586b2d2bc09422c9f230e47193db5263fdc81d
Damien Kick <dkick1@mac.com>
2022-02-05 22:44:16 -0600

    Fussing

--- py/django/imgscan/imgscan/views.py
+++ py/django/imgscan/imgscan/views.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 
 class ImageViewSet(viewsets.ModelViewSet):
-    permissions = (AllowAny,)    # No auth in spec. Maybe add later?
+    permissions = (AllowAny,)
     queryset = Image.dbobjects.all()
     serializer_class = ImageSerializer
     renderer_classes = (renderers.JSONRenderer,)
