fe3bf586917e03be63bfce21f96c69930717381a
Damien Kick <dkick1@mac.com>
2022-02-06 11:23:25 -0600

    Fussing

--- scala/play/imgscan/app/controllers/HomeController.scala
+++ scala/play/imgscan/app/controllers/HomeController.scala
@@ -9,7 +9,9 @@ import play.api.mvc._
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()
+  (val controllerComponents: ControllerComponents) extends BaseController
+{
 
   /**
    * Create an Action to render an HTML page.
