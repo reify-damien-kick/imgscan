db5f4b1e72c2696b6246a731795e1e7c4a0e119f
Damien Kick <dkick1@mac.com>
2022-02-05 20:41:52 -0600

    Some better coverage for posting data.

--- py/django/imgscan/tests/test_post.py
+++ py/django/imgscan/tests/test_post.py
@@ -34,6 +34,11 @@ def test_post_rocket():
         assert x_object in data['objects']
     objects_1 = data['objects']
 
+    filename = 'Serenity-Now-Featured-Image'
+    filetype = '.jpg'
+    fullname = F'{filename}{filetype}'
+    fullpath = os.path.join(
+        settings.BASE_DIR, 'tests', fullname)
     with open(fullpath, 'rb') as x_file:
         data = {'imgfile': (fullname, x_file, 'image/jpeg')}
         response = client.post('http://testserver/images', files=data)
@@ -48,8 +53,20 @@ def test_post_rocket():
         assert x_object in data['objects']
     objects_2 = data['objects']
 
-    assert objects_1 == objects_2
+    assert objects_1 != objects_2
 
+    response = client.get('http://testserver/images')
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) == 2
+    for image in data:
+        if image['id'] == 1:
+            assert image['objects'] == objects_1
+        elif image['id'] == 2:
+            assert image['objects'] == objects_2
+        else:
+            assert image['id'] in [1, 2]
+    
     response = client.get('http://testserver/images/1')
     assert response.status_code == 200
     data = response.json()
