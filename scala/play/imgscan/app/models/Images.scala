dbdf6388de88d99bd2aa944b96e69640ba4c8320
Damien Kick <dkick1@mac.com>
2022-02-07 11:51:03 -0600

    And a Reader[Image]

--- scala/play/imgscan/app/models/Images.scala
+++ scala/play/imgscan/app/models/Images.scala
@@ -1,15 +1,21 @@
 package models
 
+import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
 
 case class Image(val id: Int, val imgfile: String, val detect: Boolean)
 
-object ImageWrites {
+object ImageLike {
   implicit val imageWrites = new Writes[Image] {
     def writes(x: Image) = Json.obj(
       "id" -> x.id, "imgfile" -> x.imgfile, "detect" -> x.detect)
   }
+
+  implicit val imageReads: Reads[Image] = (
+    (JsPath \ "id").read[Int] and (JsPath \ "imgfile").read[String] and
+      (JsPath \ "detect").read[Boolean]
+  )(Image.apply _)
 }
 
 object Images {
