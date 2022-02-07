d6795f4541550d6a5d5422c1401117f61b371175
Damien Kick <dkick1@mac.com>
2022-02-07 11:20:41 -0600

    Add some structure to the fake objects

--- scala/play/imgscan/app/models/Images.scala
+++ scala/play/imgscan/app/models/Images.scala
@@ -1,7 +1,19 @@
 package models
 
+import play.api.libs.json._
+
+
+case class Image(val id: Int, val imgfile: String, val detect: Boolean)
+
+object Image {
+  implicit val imageWrites = new Writes[Image] {
+    def writes(x: Image) = Json.obj(
+      "id" -> x.id, "imgfile" -> x.imgfile, "detect" -> x.detect)
+  }
+}
+
 object Images {
-  def all(): Seq[String] = List(
-    "Serenity-Now-Featured-Image.jpg",
-    "Rocket-Raccoon-2.jpg")
+  def all() = List(
+    Image(id=1, imgfile="Serenity-Now-Featured-Image.jpg", detect=true),
+    Image(id=2, imgfile="Rocket-Raccoon-2.jpg", detect=true))
 }
