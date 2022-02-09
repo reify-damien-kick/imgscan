0d3f0ef0378e5b70d27ed73654436942a361a0f5
Damien Kick <dkick1@mac.com>
2022-02-09 16:52:57 -0600

    Remove PostgreSQL specific features

--- clj/imgscan/resources/migrations/20220208160318-imgscan.up.sql
+++ clj/imgscan/resources/migrations/20220208160318-imgscan.up.sql
@@ -1,26 +1,28 @@
 CREATE TABLE imgscan_image (
-  id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  id integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
   imgfile varchar(100) NOT NULL UNIQUE,
   detect bool NOT NULL,
   scanned datetime NULL);
 
 CREATE TABLE imgscan_imgobject (
-  id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  id integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
   label varchar(256) NOT NULL UNIQUE);
 
 CREATE TABLE imgscan_image_objects (
-  id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-  image_id bigint NOT NULL REFERENCES
-    imgscan_image (id) DEFERRABLE INITIALLY DEFERRED,
-  imgobject_id bigint NOT NULL REFERENCES
-    imgscan_imgobject (id) DEFERRABLE INITIALLY DEFERRED);
+  id integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  image_id integer NULL,
+  imgobject_id integer NULL,
+  FOREIGN KEY (image_id) REFERENCES
+    imgscan_image (id),
+  FOREIGN KEY (imgobject_id) REFERENCES
+    imgscan_imgobject (id));
 
-CREATE UNIQUE INDEX
-  imgscan_image_objects_image_id_imgobject_id_4857e070_uniq
-    ON imgscan_image_objects (image_id, imgobject_id);
+-- CREATE UNIQUE INDEX
+--   imgscan_image_objects_image_id_imgobject_id
+--     ON imgscan_image_objects (image_id, imgobject_id);
         
-CREATE INDEX imgscan_image_objects_image_id_b891c35f
-  ON imgscan_image_objects (image_id);
+-- CREATE INDEX imgscan_image_objects_image_id
+--   ON imgscan_image_objects (image_id);
     
-CREATE INDEX imgscan_image_objects_imgobject_id_fe509086
-  ON imgscan_image_objects (imgobject_id);
+-- CREATE INDEX imgscan_image_objects_imgobject_id
+--   ON imgscan_image_objects (imgobject_id);
