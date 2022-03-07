266e9fa9509775ade828fd11002860a3f1aadd14
Damien Kick <dkick1@mac.com>
2022-03-06 19:08:31 -0600

    Fussing

--- clj/imgscan/src/clj/imgscan/goog.clj
+++ clj/imgscan/src/clj/imgscan/goog.clj
@@ -32,7 +32,7 @@
 
 (defn ->annotations [responses]
   (let [{errors true, annotations false}
-        ,,(group-by #(.hasError %) responses)]
+        ,, (group-by #(.hasError %) responses)]
     {:annotations annotations, :errors errors}))
 
 (defn ->labels [annotation]
@@ -41,6 +41,6 @@
 
 (defn labels [imgfile]
   (let [{:keys [annotations]}
-        ,,(-> imgfile ->bytes ->image ->request ->requests
+        ,, (-> imgfile ->bytes ->image ->request ->requests
               ->response ->responses ->annotations)]
     (mapcat ->labels annotations)))
